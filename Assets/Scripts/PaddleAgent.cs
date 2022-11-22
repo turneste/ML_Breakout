910eccbbe663fbae289e57a0f8c507efa6ebe1ec
Steven Turner <71907094+turneste@users.noreply.github.com>
2022-11-21 19:12:19 -0500

    Finish Agent

--- Assets/Scripts/PaddleAgent.cs
+++ Assets/Scripts/PaddleAgent.cs
@@ -9,24 +9,18 @@ using TMPro;
 public class PaddleAgent : Agent
 {
     public GameObject BallAgent;
-    public GameObject leftWall;
-    public GameObject rightWall;
-    float leftWallPosition;
-    float rightWallPosition;
     public Vector2 direction { get; private set; }
     Rigidbody2D paddleBody;
-    float speed = 30f;
+    public float speed = 30f;
     public int currentScore;
     public int currentLives;
     public int checkScore;
     public int checkLives;
     public Vector2 startPosition;
 
-    public override void Initialize()
-    {
+    public void Start () {
         startPosition = this.transform.localPosition;
-        leftWallPosition = leftWall.transform.localPosition.x + leftWall.GetComponent<BoxCollider2D>().bounds.size.x / 2;
-        rightWallPosition = rightWall.transform.localPosition.x - rightWall.GetComponent<BoxCollider2D>().bounds.size.x / 2;
+        paddleBody = this.GetComponent<Rigidbody2D>();
     }
 
 
@@ -53,7 +47,6 @@ public class PaddleAgent : Agent
 
         // Reset Paddle
         ResetPaddle();
-        paddleBody = this.GetComponent<Rigidbody2D>();
 
         // Reset lives & Score
         currentScore = 0;
@@ -69,38 +62,19 @@ public class PaddleAgent : Agent
         // Paddle position
         sensor.AddObservation(this.transform.localPosition.x);
         sensor.AddObservation(this.transform.localPosition.y);
-
-        // Maximum values used to normalize observations
-        // float maxAngleNorm = 180f;
-        //float maxWallDist = 13.7f;
-        //float maxBallDistX = 12.2f;
-        //float maxBallDistY = 14.7f;
-        float xDist = this.transform.localPosition.x - BallAgent.transform.localPosition.x;
-        float yDist = this.transform.localPosition.y - BallAgent.transform.localPosition.y;
         
         // Distance from ball to paddle
-        sensor.AddObservation(xDist);
-        sensor.AddObservation(yDist);
+        sensor.AddObservation(this.transform.localPosition.x - BallAgent.transform.localPosition.x);
+        sensor.AddObservation(this.transform.localPosition.y - BallAgent.transform.localPosition.y);
 
         // Velocity
         sensor.AddObservation(BallAgent.GetComponent<Rigidbody2D>().velocity);
-
-        // Distance to walls
-        //sensor.AddObservation((BallAgent.localPosition.x - leftWallPosition)/maxWallDist);
-        //sensor.AddObservation((BallAgent.localPosition.x - rightWallPosition)/maxWallDist);
 
         // Angle between ball and walls
         sensor.AddObservation(Vector2.Angle(this.transform.localPosition, BallAgent.transform.localPosition));
 
     }
 
-    private float normalize(float value) {
-        return value / (1f + Mathf.Abs(value));
-    }
-
-    /// <summary>
-    /// Updates paddle position
-    /// </summary>
     private void FixedUpdate()
     {
         if (this.direction != Vector2.zero)
@@ -117,31 +91,28 @@ public class PaddleAgent : Agent
         if (moveX == 1) { this.direction = Vector2.left; }
         if (moveX == 2) { this.direction = Vector2.right; }
 
-        // Small reward for not dying
-        SetReward(.01f);
-        
-        
+        // Small penalty for not completing level
+        SetReward(-.01f);
+                
         float distanceToBall = Vector2.Distance(this.transform.localPosition, BallAgent.transform.localPosition);
         // Small reward for being close to the ball
         if (distanceToBall < this.GetComponent<BoxCollider2D>().size.x/2 && BallAgent.transform.localPosition.y > -7.5f)
         {
             SetReward(0.5f);
         }
-        
-        
-
+                
         // Large penalty for going out of bounds
         if (currentLives < checkLives)
         {
             SetReward(-1.0f);
-            EndEpisode();
-            //checkLives = currentLives;
+            //EndEpisode();
+            
+            checkLives = currentLives;
 
-            /*
             if (checkLives == 0) {
                 EndEpisode();
             }
-            */
+            
         }
 
         // Reward for breaking bricks
@@ -151,7 +122,6 @@ public class PaddleAgent : Agent
         }
 
         if (currentScore % 192 == 0 && currentScore != 0) {
-            Debug.Log("LEVEL CLEAR" + currentScore);
             EndEpisode();
         }
 
@@ -167,9 +137,4 @@ public class PaddleAgent : Agent
         
     }
 
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-    }
 }
